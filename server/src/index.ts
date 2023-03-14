e47f4a3c94995b1e5b71a32d0d6df5f6e473ae5f
Your Name <you@example.com>
2023-03-14 15:15:12 -0300

    criando servidor

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,36 @@
 import express from "express"
+import cors from "cors"
+
 
 const app = express()
-const PORT = 8080
+const PORT = 3000
 
-app.get("/", (req, res) => {res.send("Hello!")})
-app.get("/tarefa/:id", (req, res) => {res.send("Hello!")})
-app.post("/tarefa", (req, res) => {res.send("Hello!")})
-app.put("/tarefa", (req, res) => {res.send("Hello!")})
-app.patch("/tarefa/:id", (req, res) => {res.send("Hello!")})
-app.delete("/tarefa/:id", (req, res) => {res.send("Hello!")})
+app.use(cors())
+app.use("/tarefa", express.json())
 
+app.get("/tarefa", (req, res) => {
+    res.send("Hello!")
+})
+
+app.get("/tarefa/:id", (req, res) => {
+    res.send("Hello!")
+})
+
+app.post("/tarefa", (req, res) => { 
+    console.log("recebido", req.body.name)
+    res.send("Hello!")
+})
+
+app.put("/tarefa", (req, res) => {
+    res.send("Hello!")
+})
+
+app.patch("/tarefa/:id", (req, res) => {
+    res.send("Hello!")
+})
+
+app.delete("/tarefa/:id", (req, res) => {
+    res.send("Hello!")
+})
 
 app.listen(PORT, () => (`HTTP server ir running on ${PORT}`))
