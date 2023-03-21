60f58bb933d01fde1424a1967b0b44dc9efe21c0
Your Name <you@example.com>
2023-03-21 16:15:47 -0300

    Criando banco de dados

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,36 +1,9 @@
 import express from "express"
-import cors from "cors"
+import routes from "./routes/index.routes"
 
-
-const app = express()
 const PORT = 3000
+const app = express()
 
-app.use(cors())
-app.use("/tarefa", express.json())
+app.use(routes)
 
-app.get("/tarefa", (req, res) => {
-    res.send("Hello!")
-})
-
-app.get("/tarefa/:id", (req, res) => {
-    res.send("Hello!")
-})
-
-app.post("/tarefa", (req, res) => { 
-    console.log("recebido", req.body.name)
-    res.send("Hello!")
-})
-
-app.put("/tarefa", (req, res) => {
-    res.send("Hello!")
-})
-
-app.patch("/tarefa/:id", (req, res) => {
-    res.send("Hello!")
-})
-
-app.delete("/tarefa/:id", (req, res) => {
-    res.send("Hello!")
-})
-
-app.listen(PORT, () => (`HTTP server ir running on ${PORT}`))
+app.listen(PORT, () => console.log(`⚡ server is running, on port ${PORT}`))
